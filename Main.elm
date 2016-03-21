3db141a12ce02231bdede14f1bb98397c371d6be
Abey Onalaja <abey.onalaja@gmail.com>
2016-03-21 21:22:50 +0000

    Add events and start app to wire up events and model to view

--- Main.elm
+++ Main.elm
@@ -1,4 +1,6 @@
 import Html exposing(..)
+import StartApp.Simple exposing (start)
+import Html.Events exposing (onClick)
 
 type alias Model = Int
 
@@ -10,10 +12,14 @@ update action model =
   case action of
     Increment -> model + 1
     Decrement -> model - 1
+
+view : Signal.Address Action -> Model -> Html
+
+view address model =
+  div [] [
+         button [ onClick address Decrement ] [ text "-" ]
+        , div [] [text (toString model)]
+        , button [ onClick address Increment ] [ text "+" ]
+        ]
 main =
-  div []
-  [
-    button [] [text "-"]
-  , div [] [text (toString 0)]
-  , button [] [text "+"]
-  ]
+  start {model = 0, update = update, view = view}
